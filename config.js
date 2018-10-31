53d345f96ad85be67cabb5659652680ffba92e88
Caleb Bray <caleb.bray@economicmodeling.com>
2018-10-31 16:18:48 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 3000,
+  URL: process.env.BASE_URL || 'http://localhost:3000',
+  MONGODB_URI:
+    'mongodb://caleb:Elephant1!@ds243491.mlab.com:43491/fantasy-football'
+};
