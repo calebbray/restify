9485b0667c31a669182c20fb57fe6e8601eb9c78
Caleb Bray <caleb.bray@economicmodeling.com>
2018-11-01 16:43:16 -0700

    users

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 server.listen(config.PORT, () => {
+  mongoose.set('useFindAndModify', false);
   mongoose.connect(
     config.MONGODB_URI,
     { useNewUrlParser: true }
@@ -19,5 +20,6 @@ db.on('error', err => console.log(err));
 
 db.once('open', () => {
   require('./routes/customers')(server);
+  require('./routes/users')(server);
   console.log(`Server started on port ${config.PORT}`);
 });
